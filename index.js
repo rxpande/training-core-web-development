3c33a3b73a5feee23859b273703c8f73c6e2f19e
rxpande <rxpande@gmail.com>
2020-06-07 19:11:14 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 // Import stylesheets
 import './style.css';
 
+import './array.js';
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Starter</h1>
+<ul>
+
+  <li>
+    Variables, Values, Types (number, string, boolean, object, null, undefined)
+  </li>
+  <li>
+     Expressions, Operators, Statements (literals, conditions, loops)
+  </>
+  <li>Objects (create, manage properties, built-in methods, hash data structure, property descriptors)</li>
+  <li>
+    [array.js]: Arrays (create, indexes, length, modification, built-in methods: sorting, filtering, search, iterating)
+  </li>
+  <li>
+    Functions (create, invoke)
+  </li>
+  <li>
+    ES2015 Basics (const, let, arrow functions, default parameters, spread operator, destruction, string templates, for..of loop)</li>
+
+</ul>`;
