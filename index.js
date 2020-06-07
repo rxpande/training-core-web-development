3d377b9e1fbd1398607e025fbf26b4cadeb69a4b
rxpande <rxpande@gmail.com>
2020-06-07 20:49:17 +0530

    1 file modified

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import './es6-basic.js';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>
+appDiv.innerHTML = `<h1>Done : [Basic] Core JavaScript</h1>
 <ul>
 
   <li>
