d5a8a9a9eafdba6bcaf43c94db83793c7772a2b6
rxpande <rxpande@gmail.com>
2020-06-07 19:15:47 +0530

    1 file modified

--- array.js
+++ array.js
@@ -1,2 +1,26 @@
+// create //  length
 let fruits = ['Apple', 'Banana']
 console.log(fruits.length)
+
+// indexes
+console.log(fruits[1]);
+
+// modification
+fruits[0] = 'fig';
+console.log(fruits);
+
+/**
+ * built-in methods
+ */
+
+// sorting
+const fruits = fruits.sort();
+console.log(fruits);
+
+// search
+const fruit = fruits.filter( fruit => fruit === 'fig');
+console.log(fruit);
+
+// iterating
+fruits.forEach( fruit =>  console.log(fruit));
+
