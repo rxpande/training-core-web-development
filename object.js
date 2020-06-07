0d232ad11107b5f5d19bba435da8ab9ace4f2adf
rxpande <rxpande@gmail.com>
2020-06-07 19:53:08 +0530

    1 file modified

--- object.js
+++ object.js
@@ -2,19 +2,49 @@
 const person = {
  f_name: '',
  l_name: '',
- fullName: () => {
-   return this.f_name + this.l_name;
+ fullName: function() {
+  console.log(this.f_name + this.l_name);
  }
 };
 
 const me = Object.create(person);
 me.f_name = 'Rahul';
 me.l_name = 'Pande'
-const fullName = me.fullName();
-
-console.log(fullName);
+me.fullName();
 
 // manage properties, 
-// built-in methods, 
-// hash data structure, 
+Object.defineProperty(me, 'address', {
+  value: 'Pune',
+});
+console.log(me.address);
+
+// built-in methods
+for (let [key, value] of Object.entries(me)) {
+  console.log(`${key}: ${value}`);
+}
+
 // property descriptors
+
+function Archiver() {
+  var archive = [];
+  var temperature = null;
+
+  Object.defineProperty(this, 'temperature', {
+    get() {
+      console.log('get!');
+      return temperature;
+    },
+    set(value) {
+      temperature = value;
+      archive.push({ val: temperature });
+    }
+  });
+
+  this.getArchive = function() { return archive; };
+}
+
+var arc = new Archiver();
+arc.temperature; // 'get!'
+arc.temperature = 11;
+arc.temperature = 13;
+arc.getArchive(); // [{ val: 11 }, { val: 13 }]
